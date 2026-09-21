7cfa6a11bcba645fdf83d36cb37150f91fd55bed
Anurag Thakur <85978206+AnuragT2007@users.noreply.github.com>
2026-09-21 12:49:06 +0530

    Update form endpoint URL in main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
   'use strict';
 
   /* -------- settings you may want to change -------- */
-  var FORM_ENDPOINT = 'https://readdy.ai/api/form/dan3g60p3fe23ggcgm10';
+  var FORM_ENDPOINT = 'https://formsubmit.co/el/muriji';
   var ROLES = ['Visual Designer', 'Web Developer', 'Presentation Designer', 'Graphic Designer'];
   var THEME_KEY = 'portfolio-theme';
   var CONTACT_EMAIL = 'thakur.07anurag@gmail.com';
